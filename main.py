3bd629b41d8cd198665c9d15cf2170fb89e22126
wolflergf <wolflergf@gmail.com>
2022-11-17 11:08:01 +0000

    Add Functions to add books

--- main.py
+++ main.py
@@ -1,20 +1,43 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap(app)
+
+
+class Books(FlaskForm):
+    book_name = StringField("Book Name", validators=[DataRequired()])
+    book_author = StringField("Book Author", validators=[DataRequired()])
+    rating = StringField("Rating", validators=[DataRequired()])
+    submit = SubmitField("Add Book")
+
 
 all_books = []
 
 
 @app.route('/')
 def home():
-    pass
+    books = all_books
+    return render_template("index.html", books=books)
 
 
-@app.route("/add")
+@app.route("/add", methods=["POST", "GET"])
 def add():
-    pass
+    form = Books()
+    if form.validate_on_submit():
+        book = {
+            "title": form.book_name.data,
+            "author": form.book_author.data,
+            "rating": form.rating.data,
+        }
+        all_books.append(book)
+        return redirect(url_for('home'))
+    return render_template("add.html", form=form)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
