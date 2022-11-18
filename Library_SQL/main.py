8bf272a67679f1d7a6530259690201e2885bd6c6
wolflergf <wolflergf@gmail.com>
2022-11-18 22:26:57 +0000

    New version, add SQL

--- Library_SQL/main.py
+++ Library_SQL/main.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from flask_bootstrap import Bootstrap
 
 # create the extension
 db = SQLAlchemy()
@@ -10,19 +14,41 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///all_books.db"
 # initialize the app with the extension
 db.init_app(app)
 
+Bootstrap(app)
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+
+with app.app_context():
+    db.create_all()
+
 all_books = []
 
 
 @app.route('/')
 def home():
-    pass
+    return render_template("index.html")
 
 
-@app.route("/add")
+@app.route("/add", methods=["POST", "GET"])
 def add():
-    pass
+    if request.method == "POST":
+        new_book = Book(
+            title=request.form["title"],
+            author=request.form["author"],
+            rating=request.form["rating"]
+        )
+        db.session.add(new_book)
+        db.session.commit()
+        return redirect(url_for("home"))
+    return render_template("add.html")
+
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
